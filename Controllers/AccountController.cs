21e35f113328d3daaad6c658981f8741c10d4766
Mehdi <104071046+MehdiBabaei@users.noreply.github.com>
2023-10-08 17:22:25 +0330

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using DataLayer;
+using DataLayer.Services;
+using DataLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MyCms.Controllers
+{
+    public class AccountController : Controller
+    {
+        MyCmsContext db = new MyCmsContext();
+
+        private ILoginRepository loginRepository;
+        public AccountController()
+
+        {
+            loginRepository = new LoginRepository(db);
+        }
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login,string ReturnUrl="/")
+        {
+            if (ModelState.IsValid)
+            {
+                if (loginRepository.IsExistUser(login.UserName, login.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(login.UserName, login.RememberMe);
+                    return Redirect(ReturnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError("UserName", "نام کاربری یا کلمه عبور اشتباه است");
+                }
+            }
+            return View(login);
+        }
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+    }
+}
